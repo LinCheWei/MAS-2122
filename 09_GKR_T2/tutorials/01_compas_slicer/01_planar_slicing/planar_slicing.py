155a46ed6e641f29b4f7b1e417e13168b307a96a
LinCheWei <lin@dbt-ethz.ch>
2022-03-22 14:41:45 +0100

    week 1

--- 09_GKR_T2/tutorials/01_compas_slicer/01_planar_slicing/planar_slicing.py
+++ 09_GKR_T2/tutorials/01_compas_slicer/01_planar_slicing/planar_slicing.py
@@ -7,6 +7,7 @@ import compas_slicer.utilities as utils
 from compas_slicer.pre_processing import move_mesh_to_point
 from compas_slicer.post_processing import simplify_paths_rdp_igl
 from compas_slicer.post_processing import seams_smooth
+from compas_slicer.post_processing import seams_align
 from compas_slicer.print_organization import PlanarPrintOrganizer
 from compas_slicer.print_organization import set_extruder_toggle
 from compas_slicer.print_organization import add_safety_printpoints
@@ -29,19 +30,20 @@ logging.basicConfig(format='%(levelname)s-%(message)s', level=logging.INFO)
 # ==============================================================================
 DATA = os.path.join(os.path.dirname(__file__), 'data')
 OUTPUT_DIR = utils.get_output_directory(DATA)  # creates 'output' folder if it doesn't already exist
-MODEL = '...'
+MODEL = 'connection3legs_open_low_res.obj'
 
 start_time = time.time()
 
 # ==========================================================================
 # Load mesh
 # ==========================================================================
-compas_mesh = ...
+compas_mesh = Mesh.from_obj(os.path.join(DATA, MODEL))
+print(compas_mesh)
 
 # ==========================================================================
 # Move to origin
 # ==========================================================================
-...
+move_mesh_to_point(compas_mesh, Point(0,0,0))
 
 # ==========================================================================
 # Slicing
@@ -49,62 +51,65 @@ compas_mesh = ...
 #          'cgal':    Very fast. Only for closed paths.
 #                     Requires additional installation (compas_cgal).
 # ==========================================================================
-slicer = ...
-...
+slicer = PlanarSlicer(
+    mesh=compas_mesh,
+    slicer_type='cgal',
+    layer_height=3.5)
+slicer.slice_model()
 
 # ==========================================================================
 # Simplify the paths by removing points with a certain threshold
 # change the threshold value to remove more or less points
 # ==========================================================================
-...
+simplify_paths_rdp_igl(slicer, threshold=0.6)
 
 # ==========================================================================
 # Smooth the seams between layers
 # change the smooth_distance value to achieve smoother, or more abrupt seams
 # ==========================================================================
-...
+seams_smooth(slicer, smooth_distance=10)
 
 # ==========================================================================
 # Prints out the info of the slicer
 # ==========================================================================
-...
+slicer.printout_info()
 
 # ==========================================================================
 # Save slicer data to JSON
 # ==========================================================================
-...
+save_to_json(slicer.to_data(), OUTPUT_DIR, 'slicer_data.json')
 
 # ==========================================================================
 # Initializes the PlanarPrintOrganizer and creates PrintPoints
 # ==========================================================================
-print_organizer = ...
-...
+print_organizer = PlanarPrintOrganizer(slicer)
+print_organizer.create_printpoints(False)
 
 # ==========================================================================
 # Set fabrication-related parameters
 # ==========================================================================
-... #extruder toggle
-... # safety printpoints
-... # velocity
-... # blend radius
+set_extruder_toggle(print_organizer, slicer) # extruder toggle
+add_safety_printpoints(print_organizer, z_hop= 10.0) # safety printpoints
+set_linear_velocity_constant(print_organizer, v= 100) # velocity
+set_blend_radius(print_organizer, d_fillet=10) # blend radius
 
 # ==========================================================================
 # Prints out the info of the PrintOrganizer
 # ==========================================================================
-...
+print_organizer.printout_info()
 
 # ==========================================================================
 # Converts the PrintPoints to data and saves to JSON
 # =========================================================================
-printpoints_data = ...
-...
+printpoints_data = print_organizer.output_nested_printpoints_dict()
+save_to_json(printpoints_data, OUTPUT_DIR, 'out_printpoints_nested.json')
 
 # ==========================================================================
 # Initializes the compas_viewer and visualizes results
 # ==========================================================================
-viewer = ...
-...
-...
+viewer = app.App(width=1280, height=720)
+slicer.visualize_on_viewer(viewer, False, True)
+viewer.show()
 
 end_time = time.time()
 print("Total elapsed time", round(end_time - start_time, 2), "seconds")
